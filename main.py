0f69292f31ebc41d1bd30d3840f909a0e75088e8
Rene Grempel <grempelrene@gmail.com>
2026-04-24 20:44:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import os
-from logger import logger, logger_with_path
+from datetime import datetime
+from functools import wraps
 
 
-# TEST 1
+def logger(old_function):
+
+    @wraps(old_function)
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+
+        with open('main.log', 'a', encoding='utf-8') as log_file:
+            log_file.write(
+                f'{datetime.now()} | '
+                f'{old_function.__name__} | '
+                f'args={args}, kwargs={kwargs} | '
+                f'result={result}\n'
+            )
+
+        return result
+
+    return new_function
+
+
 def test_1():
 
     path = 'main.log'
@@ -22,62 +41,22 @@ def test_1():
         return a / b
 
     assert 'Hello World' == hello_world()
-    result = summator(2, 2)
-    assert result == 4
-
-    result = div(6, 2)
-    assert result == 3
+    assert summator(2, 2) == 4
+    assert div(6, 2) == 3
 
     assert os.path.exists(path)
 
     summator(4.3, b=2.2)
     summator(a=0, b=0)
 
-    with open(path) as f:
-        content = f.read()
+    with open(path, encoding='utf-8') as log_file:
+        log_file_content = log_file.read()
 
-    assert 'summator' in content
+    assert 'summator' in log_file_content
+
     for item in (4.3, 2.2, 6.5):
-        assert str(item) in content
-
-
-# TEST 2
-def test_2():
-    paths = ('log_1.log', 'log_2.log', 'log_3.log')
-
-    for path in paths:
-        if os.path.exists(path):
-            os.remove(path)
-
-        @logger_with_path(path)
-        def hello_world():
-            return 'Hello World'
-
-        @logger_with_path(path)
-        def summator(a, b=0):
-            return a + b
-
-        @logger_with_path(path)
-        def div(a, b):
-            return a / b
-
-        assert hello_world() == 'Hello World'
-        assert summator(2, 2) == 4
-        assert div(6, 2) == 3
-
-        summator(4.3, b=2.2)
-
-    for path in paths:
-        assert os.path.exists(path)
-
-        with open(path) as f:
-            content = f.read()
-
-        assert 'summator' in content
-        for item in (4.3, 2.2, 6.5):
-            assert str(item) in content
+        assert str(item) in log_file_content
 
 
 if __name__ == '__main__':
     test_1()
-    test_2()
