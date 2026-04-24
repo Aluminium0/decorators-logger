3b112450478b08622a1f1a5d028df906ab5a4794
Rene Grempel <grempelrene@gmail.com>
2026-04-24 20:38:16 +0200

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from functools import wraps
+
+
+# Обычный декоратор 
+def logger(old_function):
+
+    @wraps(old_function)
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+
+        with open('main.log', 'a', encoding='utf-8') as f:
+            f.write(
+                f"{datetime.now()} | "
+                f"{old_function.__name__} | "
+                f"args={args}, kwargs={kwargs} | "
+                f"result={result}\n"
+            )
+
+        return result
+
+    return new_function
+
+
+# Параметризованный декоратор
+def logger_with_path(path):
+
+    def __logger(old_function):
+
+        @wraps(old_function)
+        def new_function(*args, **kwargs):
+            result = old_function(*args, **kwargs)
+
+            with open(path, 'a', encoding='utf-8') as f:
+                f.write(
+                    f"{datetime.now()} | "
+                    f"{old_function.__name__} | "
+                    f"args={args}, kwargs={kwargs} | "
+                    f"result={result}\n"
+                )
+
+            return result
+
+        return new_function
+
+    return __logger
